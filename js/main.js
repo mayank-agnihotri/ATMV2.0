53e906b4dc7285983e598f668bdc31bfb797598c
mayu.agni <agni4912@gmail.com>
2017-03-08 12:24:53 +0530

    display negative value message rather than alert

--- js/main.js
+++ js/main.js
@@ -14,29 +14,29 @@ function validateField(){
         b=0;
      if($("#S_text3").val()=="")
         c=0;
-    
-    
+
+
     console.log(a + " " + b + " " + c)
     if((a<0)||(a>1000)||(b<0)||(b>1000)||(c<0)||(c>1000))
        return false;
-    else 
+    else
         return true;
     }
 
 function validateAmount(){
 
-    
-    
+
+
     var amount=$("#amount").val();
 
 
 
     if(amount<0||(amount%100!=0)){
-    alert("Please enter valid amount");
+    document.getElementById('status').value = "Please enter valid amount";
     }
     else{
       //document.getElementById('status').value = "Submit the amount";
-        
+
     }
 }
 
@@ -95,14 +95,14 @@ function Banker(Notes_2000,Notes_500,Notes_100,current_Amount,Max_limit ){
           window.logs=logs;
           console.log(window.bank)
             }
-          
+
       else{
           alert("Enter Values Accordingly");
           location.reload();
       }
       }
       function maxLimit(){
-          
+
 
           var max= $("#S_text5").val();
           console.log(max)
@@ -125,24 +125,24 @@ function withdraw(){
     console.log(log.Left_amount)
     if((log.Notes_2000==0)&&(log.Notes_500==0)&&(log.Notes_100==0)){
           document.getElementById('status').value = "No Currency Available Sorry for inconvenience!!! ";
-    
+
     } else
     if((value%100!=0)||(value<100)){
             document.getElementById('status').value = "Amount must be in multiple of 100 ";
-    
+
     }   else
     if(value>log.Left_amount){
          document.getElementById('status').value = "Amount is greater than Left Amount ";
-      
+
     }else
     if(value>bank.Max_limit){
         console.log(bank.Max_limit+" "+value)
          document.getElementById('status').value = "Amount is greater than Maximum Limit ";
         console.log(value>bank.Max_limit)
-       
-    } 
+
+    }
     else{
-        
+
     console.log(value)
     var temp=parseInt(value / 2000);
    console.log(temp>log.Notes_2000)
@@ -156,32 +156,32 @@ function withdraw(){
         console.log(rem)
     var Notes_100req=parseInt(rem/100);
     console.log(Notes_2000Req+" "+Notes_500Req+" "+ Notes_100req)
-        
+
     if(Notes_2000Req<=logs.Notes_2000&&Notes_500Req<=logs.Notes_500&&Notes_100req<=logs.Notes_100){
-        
+
         console.log( window.logs.Left_amount)
       document.getElementById('status').value = "Withraw Amount Successfully";
-    
+
     /*if(Notes_2000Req>logs.Notes_2000){
         if(logs.Notes_500*500>=Notes_2000Req*2000||logs.Notes_100*100>=Notes_2000Req*2000){
             document.getElementById('status').value = "Withraw Amount Successfully";
-           
+
         }
     }*/
-    
+
     var    leftAmount=log.Left_amount-value;
   var Notes2000=log.Notes_2000-Notes_2000Req;
    var Notes500=log.Notes_500-Notes_500Req;
    var Notes100=log.Notes_100-Notes_100req;
     window.logs.Left_amount=leftAmount
    var LOG=new Logs(value,Notes2000,Notes500,Notes100,leftAmount,"Debit")
- 
-   
+
+
 LOG.appRow();
          document.getElementById("S_text4").value=LOG.Left_amount;
 window.logs=LOG;
         console.log(LOG)
-    
+
 
     console.log(window.bank)
        }/* else if(log.Notes_2000==0){
@@ -191,23 +191,19 @@ window.logs=LOG;
                 rem=value- Notes_500Req*500
                 Notes_100req= rem/100;
              console.log(Notes_2000Req+" "+Notes_500Req+" "+ Notes_100req+"dfbj")
-    
+
             } else
         if(log.Notes_100*100>Notes_2000Req*2000){
             Notes_2000Req=0;
             Notes
-                
+
         }
     }*/
-        else 
+        else
         document.getElementById('status').value = "Amount different than Currency notes Combination ";
-    
-        
-        
+
+
+
     }
-    
+
 }
-
-
-
-     
